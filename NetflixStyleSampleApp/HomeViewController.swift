b16c81286bd06e1b9c3a445c0ae261734478068f
RaeHoon Baek <bmp117@naver.com>
2022-09-28 17:26:41 +0900

    NetflixStyleSampleApp 예제 - 기본 UI 그리기

--- NetflixStyleSampleApp/HomeViewController.swift
+++ NetflixStyleSampleApp/HomeViewController.swift
@@ -26,6 +26,10 @@ class HomeViewController: UICollectionViewController {
         //Data 설정, 가져오기
         contents = getContents()
         
+        //CollectionView Item(Cell) 설정
+        collectionView.register(ContentCollectonViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
+        collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
+        
         
     }
     
@@ -38,4 +42,54 @@ class HomeViewController: UICollectionViewController {
         return list
     }
     
+}
+
+//UICollectionView DataSource, Delegate
+extension HomeViewController {
+    
+    // 섹션당 보여질 셀의 개수
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            return contents[section].contentItem.count
+        }
+    }
+    
+    //콜렉션뷰 셀 설정
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch contents[indexPath.section].sectionType {
+        case .basic, .large:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewCell", for: indexPath) as? ContentCollectonViewCell else { return UICollectionViewCell() }
+            
+            cell.imageView.image = contents[indexPath.section].contentItem[indexPath.row].image
+            return cell
+        default:
+            return UICollectionViewCell()
+        }
+    }
+    
+    //헤더뷰 설정
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ContentCollectionViewHeader", for: indexPath) as? ContentCollectionViewHeader else { fatalError("Could not dequeue Header") }
+            headerView.sectionNameLabel.text = contents[indexPath.section].sectionName
+            return headerView
+        } else {
+            return UICollectionReusableView()
+        }
+    }
+    
+    //섹션 개수 설정
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return contents.count
+    }
+    
+    //셀 선택
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let sectionName = contents[indexPath.section].sectionName
+        print("TEST: \(sectionName)섹션의 \(indexPath.row + 1)번째 콘텐츠")
+        
+    }
 }
